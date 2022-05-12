3fdd96d96c05ee4869ac2f841ad710fc3f3ac4a4
Joshua Kaisar <joshuakaisar@Joshuas-MacBook-Pro.local>
2022-05-12 11:19:54 -0400

    new

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,19 +1,13 @@
-"""This makes the test configuration setup"""
+"""configuration"""
 # pylint: disable=redefined-outer-name
 import os
 import pytest
 from app import create_app, User
 from app.db import db
 
-#this is a good tutorial I used to fix this code to do datbase testing.
-#https://xvrdm.github.io/2017/07/03/testing-flask-sqlalchemy-database-with-pytest/
-
 @pytest.fixture()
 def application():
-    """This makes the app"""
-    #you need this one if you want to see whats in the database
-    #os.environ['FLASK_ENV'] = 'development'
-    #you need to run it in testing to pass on github
+    """app"""
     os.environ['FLASK_ENV'] = 'testing'
 
     application = create_app()
@@ -22,12 +16,10 @@ def application():
         db.create_all()
         yield application
         db.session.remove()
-        #drops the database tables after the test runs
-        #db.drop_all()
 
 @pytest.fixture()
 def add_user(application):
-    """adding user"""
+    """user"""
     with application.app_context():
         #new record
         user = User('keith@webizly.com', 'testtest', True)
@@ -36,11 +28,11 @@ def add_user(application):
 
 @pytest.fixture()
 def client(application):
-    """This makes the http client"""
+    """client"""
     return application.test_client()
 
 
 @pytest.fixture()
 def runner(application):
-    """This makes the task runner"""
+    """task"""
     return application.test_cli_runner()
